5ef9963752a1be349e74b1f32ed56b119f6542ca
konateb <111666644+konateb@users.noreply.github.com>
2023-08-01 08:01:58 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("salut")
+import prompt from "prompt-sync";
+import { getRandomID, sub , getRandom} from "./utils/helpers.js";
+
+
+// let c1 = prompt()("Enter premier: ");
+//  let c2= prompt()("Enter second: ");
+
+// let x = sub(c1, c2);
+
+//  let y = getRandomID(c1,c2);
+let r = Math.floor(getRandom());
+//  await getData(city);
+//  console.log("sub: ",x);
+
+  console.log("Aleatoire : ", r);
+
+// console.log(weatherData);
